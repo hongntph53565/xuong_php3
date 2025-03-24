8dcfe0b4e3bfd2438e5ada568d3c63da37fec100
hongntph53565 <hongnthiph53565@gmail.com>
2025-03-24 09:54:12 +0700

    first commit

--- database/migrations/2025_03_23_225758_create_reservations_table.php
+++ database/migrations/2025_03_23_225758_create_reservations_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('reservations', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('room_id');
+            $table->unsignedBigInteger('room_id');
             $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
-            $table->unsignedInteger('hotel_id');
+            $table->unsignedBigInteger('hotel_id');
             $table->foreign('hotel_id')->references('id')->on('hotels')->onDelete('cascade');
             $table->date('check_in_date');
             $table->date('check_out_date');
-            $table->price('total_price', 10, 2);
+            $table->float('total_price', 10, 2);
             $table->enum('status', ['pending', 'confirmed', 'cancelled'])->default('pending');
             $table->timestamps();
         });
